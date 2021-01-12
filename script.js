fd90d59d4b9a72de9a054da50ece23cd5f34d69c
Tobi Samuel <tobisamuel0173@gmail.com>
2021-01-12 19:49:56 +0100

    Finish project.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const menu = document.getElementById('menu');
-const nav = menu.querySelectorAll('ul li')
+const menu = document.getElementById("menu");
+const title = document.getElementById("title");
+const links = document.querySelectorAll(".link");
+// const nav = menu.querySelectorAll('ul li')
 
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
@@ -7,8 +9,20 @@ const wrapper = document.querySelector(".wrapper");
 
 sign_up_btn.addEventListener("click", () => {
   wrapper.classList.add("sign-up-mode");
+  setTimeout(() => {
+    title.classList.add("change");
+  }, 1000);
+  setTimeout(() => {
+    links.forEach(link => link.classList.add("change"));
+  }, 870);
 });
 
 sign_in_btn.addEventListener("click", () => {
   wrapper.classList.remove("sign-up-mode");
+  setTimeout(() => {
+    title.classList.remove("change");
+  }, 870);
+  setTimeout(() => {
+    links.forEach(link => link.classList.remove("change"));
+  }, 1000);
 });
